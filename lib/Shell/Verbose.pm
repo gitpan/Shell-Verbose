837fc043865e4fc00ed21c9eca4ec3a3a366ebf9
Drew Stephens <dinomite@cpan.org>
2011-10-18 19:10:00 +0000

    Import of DINOMITE/Shell-Verbose-0.1 from CPAN.
    gitpan-cpan-distribution: Shell-Verbose
    gitpan-cpan-version:      0.1
    gitpan-cpan-path:         DINOMITE/Shell-Verbose-0.1.tar.gz
    gitpan-cpan-author:       DINOMITE
    gitpan-cpan-maturity:     released

---
+++ lib/Shell/Verbose.pm
@@ -0,0 +1,88 @@
+package Shell::Verbose;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Shell::Verbose - A verbose version of system()
+
+=head1 SYNOPSIS
+
+    use Shell::Verbose qw/verboseSystem vsys/;
+
+    verboseSystem('echo "foo"');
+    # echo "foo"
+    # foo
+
+    vsys('echo "foo"');
+    # echo "foo"
+    # foo
+
+    Shell::Verbose->prefix('===> ');
+    # ===> echo 'foo'
+    # foo
+
+    Shell::Verbose->before('Running the next line');
+    # Running the next line
+    # ===> echo 'foo'
+    # foo
+
+    Shell::Verbose->after('That was easy');
+    # Running the next line
+    # ===> echo 'foo'
+    # foo
+    # That was easy
+
+=head1 DESCRIPTION
+
+A simple wrapper for system() that prints the command
+
+=head1 AUTHOR
+
+Drew Stephens <drew@dinomit.net>
+
+=cut
+
+our ($VERSION, @ISA, @EXPORT_OK);
+BEGIN {
+    $VERSION = '0.1';
+
+    require Exporter;
+    @ISA = qw/Exporter/;
+    @EXPORT_OK = qw/verboseSystem vsys/;
+}
+
+my $prefix = '';
+my $before = '';
+my $after = '';
+
+sub prefix {
+    shift;
+    $prefix = shift;
+}
+
+sub before {
+    shift;
+    $before = shift;
+}
+
+sub after {
+    shift;
+    $before = shift;
+}
+
+sub verboseSystem {
+    my $command = shift;
+
+    print "$before\n" if ($before);
+    print $prefix . $command . "\n";
+    my $ret = (system($command) == 0);
+    print "$after\n" if ($after);
+    return $ret;
+}
+
+sub vsys {
+    verboseSystem(@_);
+}
+
+1;
